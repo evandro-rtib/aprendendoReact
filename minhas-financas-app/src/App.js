a2e5ebfe0dc624acfe1000ca28acae38c7f0716d
Evandro R.  Tiburcio <evandrorti@gmail.com>
2022-09-23 17:43:41 -0300

    Exemplo fazendo soma, usando function

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -2,17 +2,39 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    nome : "Evandro"
+    num1 : null,
+    num2 : null,
+    result: null
+  }
+
+  somar = () => {
+    const result = parseInt(this.state.num1) + parseInt(this.state.num2)
+    this.setState({result : result})
   }
 
   render(){
     return(
 
       <div>
-        <label>Nome:</label>
-        <input type="text" value={this.state.nome} onChange={(e) => this.setState({nome : e.target.value})} />
-
-        O nome digitado foi: {this.state.nome}
+        <label>Primeiro número:</label>
+        <br/>
+        <input
+          type="text"
+          value={this.state.num1}
+          onChange={(e) => this.setState({num1 : e.target.value})}
+        />
+        <br/>
+        <label>Segundo número:</label>
+        <br/>
+        <input
+          type="text"
+          value={this.state.num2}
+          onChange={(e) => this.setState({num2 : e.target.value})}
+        />
+        <br/>
+        O resultado é: {this.state.result}
+        <br/>
+        <button onClick={(this.somar)} >Somar</button>
       </div>
     )
   }
