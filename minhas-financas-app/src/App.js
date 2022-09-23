523fcf9a97304f15e2341daa6d94207f70fc600d
Evandro R.  Tiburcio <evandrorti@gmail.com>
2022-09-23 18:00:22 -0300

    Intro ao bootswatch

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -1,40 +1,11 @@
 import React from 'react';
 
+import 'bootswatch/dist/flatly/bootstrap.css'
 class App extends React.Component {
-  state = {
-    num1 : null,
-    num2 : null,
-    result: null
-  }
-
-  somar = () => {
-    const result = parseInt(this.state.num1) + parseInt(this.state.num2)
-    this.setState({result : result})
-  }
-
   render(){
     return(
-
       <div>
-        <label>Primeiro número:</label>
-        <br/>
-        <input
-          type="text"
-          value={this.state.num1}
-          onChange={(e) => this.setState({num1 : e.target.value})}
-        />
-        <br/>
-        <label>Segundo número:</label>
-        <br/>
-        <input
-          type="text"
-          value={this.state.num2}
-          onChange={(e) => this.setState({num2 : e.target.value})}
-        />
-        <br/>
-        O resultado é: {this.state.result}
-        <br/>
-        <button onClick={(this.somar)} >Somar</button>
+        <button type="button" className="btn btn-success">Success</button>
       </div>
     )
   }
